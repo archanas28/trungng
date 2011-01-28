59635358efc3e8ed8951d0a42c117b59c87af4fc
trung.ngvan@gmail.com <trung.ngvan@gmail.com@5257b62d-9fc6-ae31-dabf-878df6b3b8e1>
2011-01-28 05:20:15 +0000

    Change min entity from count of frequency to count of documents

---
+++ plda/src/edu/kaist/uilab/plda/Sample.java
@@ -0,0 +1,13 @@
+package edu.kaist.uilab.plda;
+
+import java.io.Serializable;
+
+/**
+ * A sample usable in Gibbs sampling.
+ * 
+ * @author trung nguyen (trung.ngvan@gmail.com)
+ */
+public class Sample implements Serializable {
+
+  private static final long serialVersionUID = 1L;
+}
