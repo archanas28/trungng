07f339f16af357d7e9076e2e0f51002f66e066dd
trung.ngvan@gmail.com <trung.ngvan@gmail.com@5257b62d-9fc6-ae31-dabf-878df6b3b8e1>
2011-07-21 11:24:35 +0000

    minor change (moved Review to the data package).

---
+++ asc/src/edu/kaist/uilab/asc/data/Review.java
@@ -0,0 +1,60 @@
+package edu.kaist.uilab.asc.data;
+
+/**
+ * A review to be stored in the dataset.
+ * 
+ * <p> Each review contains:
+ * <li>rating (if available)
+ * <li>url (source of the review if available)
+ * <li>content
+ * </p>
+ * 
+ * @author trung nguyen (trung.ngvan@gmail.com)
+ */
+public class Review {
+  public static final double NO_RATING = -1.0;
+  
+  private Double mRating;
+  String mSource;
+  private String mContent;
+  
+  /**
+   * Constructor
+   * 
+   * <p> <code>rating</code> and <code>source</code> should be null if not available.
+   * 
+   * @param rating
+   * @param source
+   * @param content
+   */
+  public Review(String source, Double rating, String content) {
+    if (rating != null) {
+      mRating = rating;
+    } else {
+      mRating = NO_RATING;
+    }
+    if (source != null) {
+      mSource = source;
+    } else {
+      source = "";
+    }
+    mContent = content;
+  }
+  
+  public Double getRating() {
+    return mRating;
+  }
+
+  public String getContent() {
+    return mContent;
+  }
+
+  @Override
+  /**
+   * Returns a printable string which can be used to write this review into
+   * a text file.
+   */
+  public String toString() {
+    return String.format("%s\n%.1f\n%s\n", mSource, mRating, mContent);
+  }
+}
