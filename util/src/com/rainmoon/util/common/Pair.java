578ff3a1710ed8c3e26ab17275b87fe1ab42815b
trung.ngvan@gmail.com <trung.ngvan@gmail.com@5257b62d-9fc6-ae31-dabf-878df6b3b8e1>
2011-12-01 09:55:11 +0000

    misc.

--- util/src/com/rainmoon/util/common/Pair.java
+++ util/src/com/rainmoon/util/common/Pair.java
@@ -2,18 +2,34 @@ package com.rainmoon.util.common;
 /**
  * Class that represents a pair of integers.
  * 
- * @author Trung
- *
+ * @author Trung Nguyen (trung.ngvan@gmail.com)
  */
-public class Pair {
+public class Pair implements Comparable<Pair> {
   public Pair(int u, int v) {
     this.u = u;
     this.v = v;
   }
+
+  /**
+   * Compares this pair with another pair.
+   */
+  @Override
+  public int compareTo(Pair p) {
+    if (u == p.u && v == p.v) {
+      return 0;
+    }
+    
+    return u - p.u;
+  }
+
+  @Override
+  public int hashCode() {
+    return String.format("%d,%d", u, v).hashCode();
+  }
   
   @Override
   public String toString() {
-    return u + " " + v;
+    return "( " + u + "," + v + ")";
   }
   
   private int u, v;
