3ddae142bb13f61a4e3a373bb626b86d9b314ca7
trung.ngvan@gmail.com <trung.ngvan@gmail.com@5257b62d-9fc6-ae31-dabf-878df6b3b8e1>
2012-07-14 04:21:40 +0000

    - disable preference for theme - set default theme to Light and compatible with future devices

--- podcasts/default/src/com/rainmoon/podcast/MainTabActivity.java
+++ podcasts/default/src/com/rainmoon/podcast/MainTabActivity.java
@@ -29,7 +29,6 @@ import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.TabActivity;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnKeyListener;
 import android.content.Intent;
@@ -46,193 +45,221 @@ import android.widget.ScrollView;
 import android.widget.TabHost;
 import android.widget.TabHost.OnTabChangeListener;
 import android.widget.TextView;
-import com.rainmoon.podcast.R;
+
 import com.rainmoon.podcast.provider.FeedData;
 
 public class MainTabActivity extends TabActivity {
-	private static final int DIALOG_LICENSEAGREEMENT = 0;
-	
-	private boolean tabsAdded;
-	
-	private static final String TAG_NORMAL = "normal";
-	
-	private static final String TAG_ALL = "all";
-	
-	private static final String TAG_FAVORITE = "favorite";
-	
-	public static MainTabActivity INSTANCE;
-	
-	public static final boolean POSTGINGERBREAD = !Build.VERSION.RELEASE.startsWith("1") &&
-		!Build.VERSION.RELEASE.startsWith("2"); // this way around is future save
-	
-	
-	private static Boolean LIGHTTHEME;
-	
-	public static boolean isLightTheme(Context context) {
-		if (LIGHTTHEME == null) {
-			LIGHTTHEME = PreferenceManager.getDefaultSharedPreferences(context).getBoolean(Strings.SETTINGS_LIGHTTHEME, false);
-		}
-		return LIGHTTHEME;
-	}
-	
-	private Menu menu;
-	
-	private boolean hasContent;
-	
-	public void onCreate(Bundle savedInstanceState) {
-		if (isLightTheme(this)) {
-	    	setTheme(R.style.Theme_Light);
-	    }
-	    super.onCreate(savedInstanceState);
-	    setContentView(R.layout.tabs);
-	    INSTANCE = this;
-	    hasContent = false;
-        if (getPreferences(MODE_PRIVATE).getBoolean(Strings.PREFERENCE_LICENSEACCEPTED, false)) {
-        	setContent();
-        } else {
-        	/* Workaround for android issue 4499 on 1.5 devices */
-        	getTabHost().addTab(getTabHost().newTabSpec(Strings.EMPTY).setIndicator(Strings.EMPTY).setContent(new Intent(this, EmptyActivity.class)));
-        	
-        	showDialog(DIALOG_LICENSEAGREEMENT);
+  private static final int DIALOG_LICENSEAGREEMENT = 0;
+
+  private boolean tabsAdded;
+
+  private static final String TAG_NORMAL = "normal";
+
+  private static final String TAG_ALL = "all";
+
+  private static final String TAG_FAVORITE = "favorite";
+
+  public static MainTabActivity INSTANCE;
+
+  public static final boolean POSTGINGERBREAD = !Build.VERSION.RELEASE
+      .startsWith("1") && !Build.VERSION.RELEASE.startsWith("2"); // this way
+                                                                  // around is
+                                                                  // future save
+
+  private Menu menu;
+
+  private boolean hasContent;
+
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.tabs);
+    INSTANCE = this;
+    hasContent = false;
+    if (getPreferences(MODE_PRIVATE).getBoolean(
+        Strings.PREFERENCE_LICENSEACCEPTED, false)) {
+      setContent();
+    } else {
+      /* Workaround for android issue 4499 on 1.5 devices */
+      getTabHost().addTab(
+          getTabHost().newTabSpec(Strings.EMPTY).setIndicator(Strings.EMPTY)
+              .setContent(new Intent(this, EmptyActivity.class)));
+
+      showDialog(DIALOG_LICENSEAGREEMENT);
+    }
+  }
+
+  @Override
+  protected Dialog onCreateDialog(int id) {
+    AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+    builder.setIcon(android.R.drawable.ic_dialog_alert);
+    builder.setTitle(R.string.dialog_licenseagreement);
+    builder.setNegativeButton(R.string.button_decline,
+        new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.cancel();
+            finish();
+          }
+        });
+    builder.setPositiveButton(R.string.button_accept,
+        new DialogInterface.OnClickListener() {
+          public void onClick(DialogInterface dialog, int which) {
+            dialog.dismiss();
+
+            Editor editor = getPreferences(MODE_PRIVATE).edit();
+
+            editor.putBoolean(Strings.PREFERENCE_LICENSEACCEPTED, true);
+            editor.commit();
+
+            /* Part of workaround for android issue 4499 on 1.5 devices */
+            getTabHost().clearAllTabs();
+
+            /* we only want to invoke actions if the license is accepted */
+            setContent();
+          }
+        });
+    setupLicenseText(builder);
+    builder.setOnKeyListener(new OnKeyListener() {
+      public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+          dialog.cancel();
+          finish();
         }
-	}
-	
-	@Override
-	protected Dialog onCreateDialog(int id) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(this);
-		
-		builder.setIcon(android.R.drawable.ic_dialog_alert);
-		builder.setTitle(R.string.dialog_licenseagreement);
-		builder.setNegativeButton(R.string.button_decline, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.cancel();
-				finish();
-			}
-		});
-		builder.setPositiveButton(R.string.button_accept, new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int which) {
-				dialog.dismiss();
-				
-				Editor editor = getPreferences(MODE_PRIVATE).edit();
-				
-				editor.putBoolean(Strings.PREFERENCE_LICENSEACCEPTED, true);
-				editor.commit();
-				
-				/* Part of workaround for android issue 4499 on 1.5 devices */
-				getTabHost().clearAllTabs();
-				
-				/* we only want to invoke actions if the license is accepted */
-				setContent();
-			}
-		});
-		setupLicenseText(builder);
-		builder.setOnKeyListener(new OnKeyListener() {
-			public boolean onKey(DialogInterface dialog, int keyCode, KeyEvent event) {
-				if (keyCode == KeyEvent.KEYCODE_BACK) {
-					dialog.cancel();
-					finish();
-				}
-				return true;
-			}
-		});
-		return builder.create();
-	}
-	
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		this.menu = menu;
+        return true;
+      }
+    });
+    return builder.create();
+  }
 
-		Activity activity = getCurrentActivity();
-		
-		if (hasContent && activity != null) {
-			return activity.onCreateOptionsMenu(menu);
-		} else {
-			menu.add(Strings.EMPTY); // to let the menu be available
-			return true;
-		}
-	}
-	
-	@Override
-	public boolean onMenuItemSelected(int featureId, MenuItem item) {
-		Activity activity = getCurrentActivity();
-		
-		if (hasContent && activity != null) {
-			return activity.onMenuItemSelected(featureId, item);
-		} else {
-			return super.onMenuItemSelected(featureId, item);
-		}
-	}
-	
-	@Override
-	public boolean onPrepareOptionsMenu(Menu menu) {
-		Activity activity = getCurrentActivity();
-		
-		if (hasContent && activity != null) {
-			return activity.onPrepareOptionsMenu(menu);
-		} else {
-			return super.onPrepareOptionsMenu(menu);
-		}
-	}
-	
-	private void setContent() {
-	    TabHost tabHost = getTabHost();
-	    
-	    tabHost.addTab(tabHost.newTabSpec(TAG_NORMAL).setIndicator(getString(R.string.overview)).setContent(new Intent().setClass(this, RSSOverview.class)));
-	    hasContent = true;
-	    if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean(Strings.SETTINGS_SHOWTABS, false)) {
-	    	tabHost.addTab(tabHost.newTabSpec(TAG_ALL).setIndicator(getString(R.string.all)).setContent(new Intent(Intent.ACTION_VIEW, FeedData.EntryColumns.CONTENT_URI).putExtra(EntriesListActivity.EXTRA_SHOWFEEDINFO, true)));
-	    	
-		    tabHost.addTab(tabHost.newTabSpec(TAG_FAVORITE).setIndicator(getString(R.string.favorites), getResources().getDrawable(android.R.drawable.star_big_on)).setContent(new Intent(Intent.ACTION_VIEW, FeedData.EntryColumns.FAVORITES_CONTENT_URI).putExtra(EntriesListActivity.EXTRA_SHOWFEEDINFO, true).putExtra(EntriesListActivity.EXTRA_AUTORELOAD, true)));
-		    tabsAdded = true;
-		    getTabWidget().setVisibility(View.VISIBLE);
-	    }
-	    if (POSTGINGERBREAD) {
-		    /* Change the menu also on ICS when tab is changed */
-		    tabHost.setOnTabChangedListener(new OnTabChangeListener() {
-				public void onTabChanged(String tabId) {
-					if (menu != null) {
-						menu.clear();
-						onCreateOptionsMenu(menu);
-					}
-				}
-		    });
-		    if (menu != null) {
-				menu.clear();
-				onCreateOptionsMenu(menu);
-			}
-	    }
-	}
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    this.menu = menu;
 
-	public void setTabWidgetVisible(boolean visible) {
-		if (visible) {
-			if (!tabsAdded) {
-				TabHost tabHost = getTabHost();
-				
-				tabHost.addTab(tabHost.newTabSpec(TAG_ALL).setIndicator(getString(R.string.all)).setContent(new Intent(Intent.ACTION_VIEW, FeedData.EntryColumns.CONTENT_URI).putExtra(EntriesListActivity.EXTRA_SHOWFEEDINFO, true)));
-			    tabHost.addTab(tabHost.newTabSpec(TAG_FAVORITE).setIndicator(getString(R.string.favorites), getResources().getDrawable(android.R.drawable.star_big_on)).setContent(new Intent(Intent.ACTION_VIEW, FeedData.EntryColumns.FAVORITES_CONTENT_URI).putExtra(EntriesListActivity.EXTRA_SHOWFEEDINFO, true)));
-				tabsAdded = true;
-			}
-			getTabWidget().setVisibility(View.VISIBLE);
-		} else {
-			getTabWidget().setVisibility(View.GONE);
-		}
-		
-	}
-	
-	void setupLicenseText(AlertDialog.Builder builder) {
-		ScrollView scrollView = new ScrollView(this);
-		
-		TextView textView = new TextView(this);
-		
-		scrollView.addView(textView);
-		scrollView.setPadding(0, 0, 2, 0);
-		
-		textView.setTextColor(textView.getTextColors().getDefaultColor()); // disables color change on selection
-		textView.setPadding(5, 0, 5, 0);
-		textView.setTextSize(15);
-		textView.setAutoLinkMask(Linkify.EMAIL_ADDRESSES | Linkify.WEB_URLS);
-		textView.setText(new StringBuilder(getString(R.string.license_intro)).append(Strings.THREENEWLINES).append(getString(R.string.license)));
-		builder.setView(scrollView);
-	}
+    Activity activity = getCurrentActivity();
+
+    if (hasContent && activity != null) {
+      return activity.onCreateOptionsMenu(menu);
+    } else {
+      menu.add(Strings.EMPTY); // to let the menu be available
+      return true;
+    }
+  }
+
+  @Override
+  public boolean onMenuItemSelected(int featureId, MenuItem item) {
+    Activity activity = getCurrentActivity();
+
+    if (hasContent && activity != null) {
+      return activity.onMenuItemSelected(featureId, item);
+    } else {
+      return super.onMenuItemSelected(featureId, item);
+    }
+  }
+
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    Activity activity = getCurrentActivity();
+
+    if (hasContent && activity != null) {
+      return activity.onPrepareOptionsMenu(menu);
+    } else {
+      return super.onPrepareOptionsMenu(menu);
+    }
+  }
+
+  private void setContent() {
+    TabHost tabHost = getTabHost();
+
+    tabHost.addTab(tabHost.newTabSpec(TAG_NORMAL)
+        .setIndicator(getString(R.string.overview))
+        .setContent(new Intent().setClass(this, RSSOverview.class)));
+    hasContent = true;
+    if (PreferenceManager.getDefaultSharedPreferences(this).getBoolean(
+        Strings.SETTINGS_SHOWTABS, false)) {
+      tabHost.addTab(tabHost
+          .newTabSpec(TAG_ALL)
+          .setIndicator(getString(R.string.all))
+          .setContent(
+              new Intent(Intent.ACTION_VIEW, FeedData.EntryColumns.CONTENT_URI)
+                  .putExtra(EntriesListActivity.EXTRA_SHOWFEEDINFO, true)));
+
+      tabHost.addTab(tabHost
+          .newTabSpec(TAG_FAVORITE)
+          .setIndicator(getString(R.string.favorites),
+              getResources().getDrawable(android.R.drawable.star_big_on))
+          .setContent(
+              new Intent(Intent.ACTION_VIEW,
+                  FeedData.EntryColumns.FAVORITES_CONTENT_URI).putExtra(
+                  EntriesListActivity.EXTRA_SHOWFEEDINFO, true).putExtra(
+                  EntriesListActivity.EXTRA_AUTORELOAD, true)));
+      tabsAdded = true;
+      getTabWidget().setVisibility(View.VISIBLE);
+    }
+    if (POSTGINGERBREAD) {
+      /* Change the menu also on ICS when tab is changed */
+      tabHost.setOnTabChangedListener(new OnTabChangeListener() {
+        public void onTabChanged(String tabId) {
+          if (menu != null) {
+            menu.clear();
+            onCreateOptionsMenu(menu);
+          }
+        }
+      });
+      if (menu != null) {
+        menu.clear();
+        onCreateOptionsMenu(menu);
+      }
+    }
+  }
+
+  public void setTabWidgetVisible(boolean visible) {
+    if (visible) {
+      if (!tabsAdded) {
+        TabHost tabHost = getTabHost();
+
+        tabHost.addTab(tabHost
+            .newTabSpec(TAG_ALL)
+            .setIndicator(getString(R.string.all))
+            .setContent(
+                new Intent(Intent.ACTION_VIEW,
+                    FeedData.EntryColumns.CONTENT_URI).putExtra(
+                    EntriesListActivity.EXTRA_SHOWFEEDINFO, true)));
+        tabHost.addTab(tabHost
+            .newTabSpec(TAG_FAVORITE)
+            .setIndicator(getString(R.string.favorites),
+                getResources().getDrawable(android.R.drawable.star_big_on))
+            .setContent(
+                new Intent(Intent.ACTION_VIEW,
+                    FeedData.EntryColumns.FAVORITES_CONTENT_URI).putExtra(
+                    EntriesListActivity.EXTRA_SHOWFEEDINFO, true)));
+        tabsAdded = true;
+      }
+      getTabWidget().setVisibility(View.VISIBLE);
+    } else {
+      getTabWidget().setVisibility(View.GONE);
+    }
+
+  }
+
+  void setupLicenseText(AlertDialog.Builder builder) {
+    ScrollView scrollView = new ScrollView(this);
+
+    TextView textView = new TextView(this);
+
+    scrollView.addView(textView);
+    scrollView.setPadding(0, 0, 2, 0);
+
+    textView.setTextColor(textView.getTextColors().getDefaultColor()); // disables
+                                                                       // color
+                                                                       // change
+                                                                       // on
+                                                                       // selection
+    textView.setPadding(5, 0, 5, 0);
+    textView.setTextSize(15);
+    textView.setAutoLinkMask(Linkify.EMAIL_ADDRESSES | Linkify.WEB_URLS);
+    textView.setText(new StringBuilder(getString(R.string.license_intro))
+        .append(Strings.THREENEWLINES).append(getString(R.string.license)));
+    builder.setView(scrollView);
+  }
 
 }
---
+++ podcasts/default/src/com/rainmoon/podcast/StaticMethods.java
@@ -0,0 +1,11 @@
+package com.rainmoon.podcast;
+
+
+/**
+ * Provides helper static methods.
+ * 
+ * @author trung nguyen
+ * 
+ */
+public class StaticMethods {
+}
