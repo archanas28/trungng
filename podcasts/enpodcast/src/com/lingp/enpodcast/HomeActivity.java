85e563a63c792e304a8381458e8b2e8aa3f9bb36
trung.ngvan@gmail.com <trung.ngvan@gmail.com@5257b62d-9fc6-ae31-dabf-878df6b3b8e1>
2012-09-29 13:13:27 +0000

    first import for english podcast

---
+++ podcasts/enpodcast/src/com/lingp/enpodcast/HomeActivity.java
@@ -0,0 +1,42 @@
+package com.lingp.enpodcast;
+
+import com.lingp.enpodcast.R;
+
+import android.app.Dialog;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+/**
+ * The home activity of the application.
+ * 
+ * @author trung nguyen
+ * 
+ */
+public class HomeActivity extends FragmentActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_home);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    super.onCreateOptionsMenu(menu);
+    getMenuInflater().inflate(R.menu.all_subscriptions, menu);
+    return true;
+  }
+
+  public boolean onOptionsItemSelected(final MenuItem item) {
+    super.onOptionsItemSelected(item);
+    return SubscriptionsMenuHelper.onOptionsItemSelected(this, item);
+  }
+
+  @Override
+  protected Dialog onCreateDialog(int id) {
+    return SubscriptionsMenuHelper.onCreateDialog(this, id);
+  }
+}
