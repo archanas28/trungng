411e1a6d07889c53f656abd219aa98ba1569a3a3
trung.ngvan@gmail.com <trung.ngvan@gmail.com@5257b62d-9fc6-ae31-dabf-878df6b3b8e1>
2012-10-08 14:20:47 +0000

    first import for krpodcast

---
+++ podcasts/krpodcast/src/com/lingp/krpodcast/AllSubscriptionsFragment.java
@@ -0,0 +1,213 @@
+package com.lingp.krpodcast;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.AlertDialog.Builder;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.app.ListFragment;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v4.widget.CursorAdapter;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnCreateContextMenuListener;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.lingp.krpodcast.R;
+import com.lingp.krpodcast.provider.FeedData;
+import com.lingp.krpodcast.service.RefreshService;
+import com.lingp.krpodcast.utils.Strings;
+
+/**
+ * Fragment showing the list of all subscriptions.
+ * 
+ * @author trung nguyen
+ * 
+ */
+public class AllSubscriptionsFragment extends ListFragment implements LoaderCallbacks<Cursor> {
+
+  private static final int CONTEXTMENU_EDIT_ID = 1;
+  private static final int CONTEXTMENU_UNSUBSCRIBE = 2;
+  private static final String TAG = "AllSubscriptionsFragment";
+
+  private CursorAdapter mAdapter;
+  private Context mContext;
+  private BroadcastReceiver mDatabaseReadyReceiver;
+
+  /** Called when the activity is first created. */
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    mContext = getActivity();
+    boolean databaseReady = PreferenceManager.getDefaultSharedPreferences(mContext).getBoolean(
+        Strings.PREFERENCE_DATABASE_READY, false);
+    // listen for the database ready event and refresh (this is needed for first
+    // use only)
+    if (!databaseReady) {
+      mDatabaseReadyReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+          Log.d(TAG, "Database ready intent received");
+          getLoaderManager().restartLoader(0, null, AllSubscriptionsFragment.this);
+        }
+      };
+      LocalBroadcastManager.getInstance(mContext).registerReceiver(mDatabaseReadyReceiver,
+          new IntentFilter(Strings.INTENT_DATABASE_READY));
+    }
+  }
+
+  @Override
+  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    super.onCreateView(inflater, container, savedInstanceState);
+    return inflater.inflate(R.layout.view_list, container, false);
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    ListView lv = getListView();
+    lv.setOnCreateContextMenuListener(new MyContextMenuListener());
+
+    // Create an empty adapter we will use to display the loaded data.
+    mAdapter = new SubscriptionsListAdapter((Activity) mContext);
+    setListAdapter(mAdapter);
+
+    // Prepare the loader. Either re-connect with an existing one,
+    // or start a new one. The third argument is an implementation of the
+    // LoaderCallback.
+    getLoaderManager().initLoader(0, null, this);
+    if (PreferenceManager.getDefaultSharedPreferences(mContext).getBoolean(
+        Strings.SETTINGS_REFRESHENABLED, false)) {
+      // starts the service independent of this activity
+      mContext.startService(new Intent(mContext, RefreshService.class));
+      mContext.sendBroadcast(new Intent(Strings.ACTION_REFRESHFEEDS));
+    } else {
+      mContext.stopService(new Intent(mContext, RefreshService.class));
+    }
+  }
+
+  public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    // Now create and return a CursorLoader that will take care of
+    // creating a Cursor for the data being displayed.
+    return new CursorLoader(getActivity(), FeedData.SubscriptionColumns.CONTENT_URI,
+        SubscriptionsListAdapter.FROM, null, null, null);
+  }
+
+  public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    // Swap the new cursor in. This is like calling the adapter's constructor
+    // using the new cursor.
+    mAdapter.swapCursor(data);
+    ((SubscriptionsListAdapter) mAdapter).initColumnIdx(getActivity());
+    Log.i(TAG, "cursor loaded");
+  }
+
+  public void onLoaderReset(Loader<Cursor> loader) {
+    // This is called when the last Cursor provided to onLoadFinished()
+    // above is about to be closed. We need to make sure we are no longer using
+    // it.
+    mAdapter.swapCursor(null);
+  }
+
+  final class MyContextMenuListener implements OnCreateContextMenuListener {
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+      menu.setHeaderTitle(((TextView) ((AdapterView.AdapterContextMenuInfo) menuInfo).targetView
+          .findViewById(android.R.id.text1)).getText());
+      menu.add(Menu.NONE, CONTEXTMENU_EDIT_ID, Menu.NONE, R.string.contextmenu_edit);
+      menu.add(Menu.NONE, CONTEXTMENU_UNSUBSCRIBE, Menu.NONE, R.string.contextmenu_unsubscribe);
+    }
+
+  }
+
+  @Override
+  public boolean onContextItemSelected(final MenuItem item) {
+    super.onContextItemSelected(item);
+
+    switch (item.getItemId()) {
+    case CONTEXTMENU_EDIT_ID: {
+      Intent intent = new Intent(getActivity(), EditSubscriptionActivity.class);
+      intent.setData(FeedData.SubscriptionColumns
+          .subscriptionContentUri(((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).id));
+      intent.putExtra(EditSubscriptionActivity.ADD_FEED, false);
+      startActivity(intent);
+      break;
+    }
+    case CONTEXTMENU_UNSUBSCRIBE: {
+      String id = Long.toString(((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).id);
+      unsubscribe(item, id);
+      break;
+    }
+    }
+    return true;
+  }
+
+  /**
+   * Handler for Delete item.
+   * 
+   * @param item
+   * @param id
+   */
+  private void unsubscribe(final MenuItem item, String id) {
+    Cursor cursor = getActivity().getContentResolver().query(
+        FeedData.SubscriptionColumns.subscriptionContentUri(id),
+        new String[] { FeedData.SubscriptionColumns.NAME }, null, null, null);
+    cursor.moveToFirst();
+
+    Builder builder = new AlertDialog.Builder(mContext);
+
+    builder.setIcon(android.R.drawable.ic_dialog_alert);
+    builder.setTitle(cursor.getString(0));
+    builder.setMessage(R.string.question_deletefeed);
+    builder.setPositiveButton(android.R.string.yes, new DialogInterface.OnClickListener() {
+      public void onClick(DialogInterface dialog, int which) {
+        new Thread() {
+          public void run() {
+            getActivity().getContentResolver().delete(
+                FeedData.SubscriptionColumns.subscriptionContentUri(Long
+                    .toString(((AdapterView.AdapterContextMenuInfo) item.getMenuInfo()).id)), null,
+                null);
+          }
+        }.start();
+      }
+    });
+    builder.setNegativeButton(android.R.string.no, null);
+    cursor.close();
+    builder.show();
+  }
+
+  @Override
+  public void onListItemClick(ListView listView, View view, int position, long id) {
+    super.onListItemClick(listView, view, position, id);
+    Intent intent = new Intent(getActivity().getApplicationContext(),
+        SingleSubscriptionActivity.class);
+    intent.setData(FeedData.ItemColumns.subscriptionItemsContentUri(Long.toString(id))).putExtra(
+        FeedData.SubscriptionColumns._ID, id);
+    intent.putExtra(Strings.NO_CONTENT_MSG, getString(R.string.noentries));
+    startActivity(intent);
+  }
+
+  @Override
+  public void onDestroy() {
+    if (mDatabaseReadyReceiver != null)
+      LocalBroadcastManager.getInstance(getActivity()).unregisterReceiver(mDatabaseReadyReceiver);
+    super.onDestroy();
+  }
+}
---
+++ podcasts/krpodcast/src/com/lingp/krpodcast/AlwaysOnMediaController.java
@@ -0,0 +1,337 @@
+package com.lingp.krpodcast;
+
+/*
+ * Copyright (C) 2006 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import java.util.Formatter;
+import java.util.Locale;
+
+import com.lingp.krpodcast.R;
+
+import android.content.Context;
+import android.os.Handler;
+import android.os.Message;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.MediaController.MediaPlayerControl;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+
+/**
+ * A view containing controls for a MediaPlayer. Typically contains the buttons
+ * like "Play/Pause", "Rewind", "Fast Forward" and a progress slider. It takes
+ * care of synchronizing the controls with the state of the MediaPlayer.
+ * <p>
+ * The way to use this class is to instantiate it programatically. The
+ * MediaController will create a default set of controls and put them in a frame
+ * as a regular view in your application.
+ * <p>
+ */
+public class AlwaysOnMediaController extends FrameLayout {
+
+  private MediaPlayerControl mPlayer;
+  private Context mContext;
+  private View mRoot;
+  private ProgressBar mProgress;
+  private TextView mEndTime, mCurrentTime;
+  private boolean mDragging;
+  private static final int SHOW_PROGRESS = 2;
+  StringBuilder mFormatBuilder;
+  Formatter mFormatter;
+  private ImageButton mPauseButton;
+  private ImageButton mFfwdButton;
+  private ImageButton mRewButton;
+
+  @Override
+  public void onFinishInflate() {
+    if (mRoot != null)
+      initControllerView(mRoot);
+  }
+
+  public AlwaysOnMediaController(Context context) {
+    super(context);
+    mContext = context;
+    setFocusable(true);
+    setFocusableInTouchMode(true);
+    setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS);
+    // requestFocus();
+    View v = makeControllerView();
+    FrameLayout.LayoutParams frameParams = new FrameLayout.LayoutParams(
+        ViewGroup.LayoutParams.MATCH_PARENT,
+        ViewGroup.LayoutParams.MATCH_PARENT);
+    addView(v, frameParams);
+  }
+
+  /**
+   * Create the view that holds the widgets that control playback.
+   * 
+   * @return The controller view.
+   */
+  protected View makeControllerView() {
+    LayoutInflater inflate = (LayoutInflater) mContext
+        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    mRoot = inflate.inflate(R.layout.media_controller, null);
+    initControllerView(mRoot);
+    return mRoot;
+  }
+
+  private void initControllerView(View v) {
+    mPauseButton = (ImageButton) v.findViewById(R.id.btn_play);
+    mPauseButton.setOnClickListener(mPauseListener);
+    mFfwdButton = (ImageButton) v.findViewById(R.id.btn_fw);
+    mFfwdButton.setOnClickListener(mFfwdListener);
+    mRewButton = (ImageButton) v.findViewById(R.id.btn_rw);
+    mRewButton.setOnClickListener(mRewListener);
+
+    mProgress = (ProgressBar) v.findViewById(R.id.mc_seekbar);
+    if (mProgress != null) {
+      if (mProgress instanceof SeekBar) {
+        SeekBar seeker = (SeekBar) mProgress;
+        seeker.setOnSeekBarChangeListener(mSeekListener);
+      }
+      mProgress.setMax(1000);
+    }
+
+    mEndTime = (TextView) v.findViewById(R.id.time);
+    mCurrentTime = (TextView) v.findViewById(R.id.time_current);
+    mFormatBuilder = new StringBuilder();
+    mFormatter = new Formatter(mFormatBuilder, Locale.getDefault());
+  }
+
+  public void setMediaPlayer(MediaPlayerControl player) {
+    mPlayer = player;
+  }
+
+  /**
+   * Show the controller on screen.
+   */
+  public void show() {
+    updatePausePlay();
+    setProgress();
+    mHandler.sendEmptyMessage(SHOW_PROGRESS);
+  }
+
+  private Handler mHandler = new Handler() {
+    @Override
+    public void handleMessage(Message msg) {
+      try {
+        if (!mDragging && mPlayer.isPlaying()) {
+          int pos = setProgress();
+          msg = obtainMessage(SHOW_PROGRESS);
+          // update progress bar every second
+          sendMessageDelayed(msg, 1000 - (pos % 1000));
+        }
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+  };
+
+  private String stringForTime(int timeMs) {
+    int totalSeconds = timeMs / 1000;
+
+    int seconds = totalSeconds % 60;
+    int minutes = (totalSeconds / 60) % 60;
+    int hours = totalSeconds / 3600;
+
+    mFormatBuilder.setLength(0);
+    if (hours > 0) {
+      return mFormatter.format("%d:%02d:%02d", hours, minutes, seconds)
+          .toString();
+    } else {
+      return mFormatter.format("%02d:%02d", minutes, seconds).toString();
+    }
+  }
+
+  private int setProgress() {
+    if (mPlayer == null || mDragging) {
+      return 0;
+    }
+    int position = mPlayer.getCurrentPosition();
+    int duration = mPlayer.getDuration();
+    if (mProgress != null) {
+      if (duration > 0) {
+        // use long to avoid overflow
+        long pos = 1000L * position / duration;
+        mProgress.setProgress((int) pos);
+      }
+      int percent = mPlayer.getBufferPercentage();
+      mProgress.setSecondaryProgress(percent * 10);
+    }
+
+    if (mEndTime != null)
+      mEndTime.setText(stringForTime(duration));
+    if (mCurrentTime != null)
+      mCurrentTime.setText(stringForTime(position));
+
+    return position;
+  }
+
+  @Override
+  public boolean dispatchKeyEvent(KeyEvent event) {
+    int keyCode = event.getKeyCode();
+    final boolean uniqueDown = event.getRepeatCount() == 0
+        && event.getAction() == KeyEvent.ACTION_DOWN;
+    if (keyCode == KeyEvent.KEYCODE_HEADSETHOOK
+        || keyCode == KeyEvent.KEYCODE_MEDIA_PLAY_PAUSE
+        || keyCode == KeyEvent.KEYCODE_SPACE) {
+      if (uniqueDown) {
+        doPauseResume();
+        if (mPauseButton != null) {
+          mPauseButton.requestFocus();
+        }
+      }
+      return true;
+    } else if (keyCode == KeyEvent.KEYCODE_MEDIA_PLAY) {
+      if (uniqueDown && !mPlayer.isPlaying()) {
+        mPlayer.start();
+        updatePausePlay();
+      }
+      return true;
+    } else if (keyCode == KeyEvent.KEYCODE_MEDIA_STOP
+        || keyCode == KeyEvent.KEYCODE_MEDIA_PAUSE) {
+      if (uniqueDown && mPlayer.isPlaying()) {
+        mPlayer.pause();
+        updatePausePlay();
+      }
+      return true;
+    } else if (keyCode == KeyEvent.KEYCODE_VOLUME_DOWN
+        || keyCode == KeyEvent.KEYCODE_VOLUME_UP
+        || keyCode == KeyEvent.KEYCODE_VOLUME_MUTE) {
+      // don't show the controls for volume adjustment
+      return super.dispatchKeyEvent(event);
+    }
+
+    return super.dispatchKeyEvent(event);
+  }
+
+  private View.OnClickListener mPauseListener = new View.OnClickListener() {
+    public void onClick(View v) {
+      doPauseResume();
+    }
+  };
+
+  private void updatePausePlay() {
+    if (mRoot == null || mPauseButton == null)
+      return;
+
+    if (mPlayer.isPlaying()) {
+      mPauseButton.setImageResource(android.R.drawable.ic_media_pause);
+    } else {
+      mPauseButton.setImageResource(android.R.drawable.ic_media_play);
+    }
+  }
+
+  private void doPauseResume() {
+    if (mPlayer.isPlaying()) {
+      mPlayer.pause();
+    } else {
+      mPlayer.start();
+    }
+    updatePausePlay();
+  }
+
+  // There are two scenarios that can trigger the seekbar listener to trigger:
+  //
+  // The first is the user using the touchpad to adjust the posititon of the
+  // seekbar's thumb. In this case onStartTrackingTouch is called followed by
+  // a number of onProgressChanged notifications, concluded by
+  // onStopTrackingTouch.
+  // We're setting the field "mDragging" to true for the duration of the
+  // dragging
+  // session to avoid jumps in the position in case of ongoing playback.
+  //
+  // The second scenario involves the user operating the scroll ball, in this
+  // case there WON'T BE onStartTrackingTouch/onStopTrackingTouch notifications,
+  // we will simply apply the updated position without suspending regular
+  // updates.
+  private OnSeekBarChangeListener mSeekListener = new OnSeekBarChangeListener() {
+    public void onStartTrackingTouch(SeekBar bar) {
+      mDragging = true;
+      // By removing these pending progress messages we make sure
+      // that a) we won't update the progress while the user adjusts
+      // the seekbar and b) once the user is done dragging the thumb
+      // we will post one of these messages to the queue again and
+      // this ensures that there will be exactly one message queued up.
+      mHandler.removeMessages(SHOW_PROGRESS);
+    }
+
+    public void onProgressChanged(SeekBar bar, int progress, boolean fromuser) {
+      if (!fromuser) {
+        // We're not interested in programmatically generated changes to
+        // the progress bar's position.
+        return;
+      }
+
+      long duration = mPlayer.getDuration();
+      long newposition = (duration * progress) / 1000L;
+      mPlayer.seekTo((int) newposition);
+      if (mCurrentTime != null)
+        mCurrentTime.setText(stringForTime((int) newposition));
+    }
+
+    public void onStopTrackingTouch(SeekBar bar) {
+      mDragging = false;
+      setProgress();
+      updatePausePlay();
+      // Ensure that progress is properly updated in the future,
+      // the call to show() does not guarantee this because it is a
+      // no-op if we are already showing.
+      mHandler.sendEmptyMessage(SHOW_PROGRESS);
+    }
+  };
+
+  @Override
+  public void setEnabled(boolean enabled) {
+    if (mPauseButton != null) {
+      mPauseButton.setEnabled(enabled);
+    }
+    if (mFfwdButton != null) {
+      mFfwdButton.setEnabled(enabled);
+    }
+    if (mRewButton != null) {
+      mRewButton.setEnabled(enabled);
+    }
+    if (mProgress != null) {
+      mProgress.setEnabled(enabled);
+    }
+    super.setEnabled(enabled);
+  }
+
+  private View.OnClickListener mRewListener = new View.OnClickListener() {
+    public void onClick(View v) {
+      int pos = mPlayer.getCurrentPosition();
+      pos -= 5000; // milliseconds
+      mPlayer.seekTo(pos);
+      setProgress();
+    }
+  };
+
+  private View.OnClickListener mFfwdListener = new View.OnClickListener() {
+    public void onClick(View v) {
+      int pos = mPlayer.getCurrentPosition();
+      pos += 15000; // milliseconds
+      mPlayer.seekTo(pos);
+      setProgress();
+    }
+  };
+}
