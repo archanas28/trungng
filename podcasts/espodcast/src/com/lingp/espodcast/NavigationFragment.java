7c57b04f6a887a20943332dcc56db3a9897b813c
trung.ngvan@gmail.com <trung.ngvan@gmail.com@5257b62d-9fc6-ae31-dabf-878df6b3b8e1>
2012-12-29 00:25:46 +0000

    - add Randomize playing tab (this is up-to-date with other apps)

--- podcasts/espodcast/src/com/lingp/espodcast/NavigationFragment.java
+++ podcasts/espodcast/src/com/lingp/espodcast/NavigationFragment.java
@@ -5,12 +5,15 @@ import java.util.Date;
 import java.util.List;
 
 import android.app.Dialog;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.database.Cursor;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.text.format.DateUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,8 +21,8 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import com.lingp.espodcast.R;
 import com.lingp.espodcast.provider.FeedData;
 import com.lingp.espodcast.utils.Strings;
 
@@ -28,7 +31,6 @@ import com.lingp.espodcast.utils.Strings;
  * application.
  * 
  * @author trung nguyen
- * 
  */
 public class NavigationFragment extends ListFragment {
 
@@ -37,10 +39,9 @@ public class NavigationFragment extends ListFragment {
   private static final int HIGH_ACTIVITY = 100;
 
   @Override
-  public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+  public View onCreateView(LayoutInflater inflater, ViewGroup container,
+      Bundle savedInstanceState) {
     super.onCreateView(inflater, container, savedInstanceState);
-    // we don't want the default onCreateView that returns only a ListView
-    // as we want to supply the empty text
     return inflater.inflate(R.layout.fragment_navigation, container, false);
   }
 
@@ -52,21 +53,26 @@ public class NavigationFragment extends ListFragment {
     List<String> navigation = new ArrayList<String>();
     final String all = getResources().getString(R.string.all_items);
     final String favorites = getResources().getString(R.string.favorites);
-    final String recentlyViewed = getResources().getString(R.string.recently_viewed);
-    final String subscriptions = getResources().getString(R.string.subscriptions);
+    final String recentlyViewed = getResources().getString(
+        R.string.recently_viewed);
+    final String subscriptions = getResources().getString(
+        R.string.subscriptions);
     final String summary = getResources().getString(R.string.listen_summary);
+    final String random = getResources().getString(R.string.random);
 
-    // TODO: add explore for 2nd release
     navigation.add(subscriptions);
     navigation.add(all);
     navigation.add(favorites);
     navigation.add(recentlyViewed);
+    navigation.add(random);
     navigation.add(summary);
     // have to setListAdapter() here
-    setListAdapter(new ArrayAdapter<String>(getActivity(), R.layout.navigation_item, navigation));
+    setListAdapter(new ArrayAdapter<String>(getActivity(),
+        R.layout.navigation_item, navigation));
 
     getListView().setOnItemClickListener(new OnItemClickListener() {
-      public void onItemClick(AdapterView<?> list, View view, int arg2, long arg3) {
+      public void onItemClick(AdapterView<?> list, View view, int arg2,
+          long arg3) {
         TextView item = (TextView) view;
         Intent intent = null;
         String text = item.getText().toString();
@@ -74,18 +80,23 @@ public class NavigationFragment extends ListFragment {
           intent = new Intent(getActivity(), AllSubscriptionsActivity.class);
         } else if (text.equals(summary)) {
           showSummaryDialog();
+        } else if (text.equals(random)) {
+          playRandomItem();
         } else {
           intent = new Intent(getActivity().getApplicationContext(),
               SingleSubscriptionActivity.class);
           if (text.equals(all)) {
             intent.setData(FeedData.ItemColumns.ALL_ITEMS_CONTENT_URI);
-            intent.putExtra(Strings.NO_CONTENT_MSG, getString(R.string.no_content_all_items));
+            intent.putExtra(Strings.NO_CONTENT_MSG,
+                getString(R.string.no_content_all_items));
           } else if (text.equals(favorites)) {
             intent.setData(FeedData.ItemColumns.FAVORITES_CONTENT_URI);
-            intent.putExtra(Strings.NO_CONTENT_MSG,  getString(R.string.no_content_favorites));
+            intent.putExtra(Strings.NO_CONTENT_MSG,
+                getString(R.string.no_content_favorites));
           } else if (text.equals(recentlyViewed)) {
             intent.setData(FeedData.ItemColumns.RECENTLY_VIEWED_CONTENT_URI);
-            intent.putExtra(Strings.NO_CONTENT_MSG, getString(R.string.no_content_recent));
+            intent.putExtra(Strings.NO_CONTENT_MSG,
+                getString(R.string.no_content_recent));
           }
         }
         if (intent != null) {
@@ -97,30 +108,63 @@ public class NavigationFragment extends ListFragment {
 
   private void showSummaryDialog() {
     Context context = getActivity();
-    mPrefs = context.getSharedPreferences(Strings.APPLICATION_SHARED_PREFERENCES, 0);
+    mPrefs = context.getSharedPreferences(
+        Strings.APPLICATION_SHARED_PREFERENCES, 0);
     Dialog dialog = new Dialog(context);
     dialog.setTitle(R.string.title_summary);
     dialog.setContentView(R.layout.dialog_summary);
-    setWeekSummary(context, (TextView) dialog.findViewById(R.id.cnt_week_items),
+    setWeekSummary(context,
+        (TextView) dialog.findViewById(R.id.cnt_week_items),
         (TextView) dialog.findViewById(R.id.cnt_week_hours));
-    setMonthSummary(context, (TextView) dialog.findViewById(R.id.cnt_month_items),
+    setMonthSummary(context,
+        (TextView) dialog.findViewById(R.id.cnt_month_items),
         (TextView) dialog.findViewById(R.id.cnt_month_hours));
-    setTotalSummary(context, (TextView) dialog.findViewById(R.id.cnt_total_items),
+    setTotalSummary(context,
+        (TextView) dialog.findViewById(R.id.cnt_total_items),
         (TextView) dialog.findViewById(R.id.cnt_total_hours));
-    setAverageSummary(context, (TextView) dialog.findViewById(R.id.cnt_average_items),
+    setAverageSummary(context,
+        (TextView) dialog.findViewById(R.id.cnt_average_items),
         (TextView) dialog.findViewById(R.id.cnt_average_hours));
     dialog.setCanceledOnTouchOutside(true);
 
     dialog.show();
   }
 
+  private void playRandomItem() {
+    Intent intent = new Intent(getActivity(), FeedItemActivity2.class);
+    // get random play url
+    long id = 1;
+    String[] projection = new String[] { FeedData.ItemColumns._ID,
+        FeedData.ItemColumns.ENCLOSURE };
+    String selection = "enclosure != ?";
+    String[] selectionArgs = { "" };
+    Cursor cursor = getActivity().getContentResolver().query(
+        FeedData.ItemColumns.ALL_ITEMS_CONTENT_URI, projection, selection,
+        selectionArgs, null);
+    if (cursor != null && cursor.getCount() > 0) {
+      int randomPosition = (int) (cursor.getCount() * Math.random());
+      if (cursor.moveToPosition(randomPosition)) {
+        id = cursor.getInt(cursor.getColumnIndex(FeedData.ItemColumns._ID));
+      }
+      intent.setData(ContentUris.withAppendedId(
+          FeedData.ItemColumns.ALL_ITEMS_CONTENT_URI, id));
+      Log.i("NavigationFragment", "Playing " + intent.getDataString());
+      startActivity(intent);
+    } else {
+      Toast.makeText(getActivity(), R.string.randomize_no_item,
+          Toast.LENGTH_SHORT).show();
+    }
+  }
+
   /**
    * Average items and hours listened daily
    */
-  private void setAverageSummary(Context context, TextView itemview, TextView hourview) {
+  private void setAverageSummary(Context context, TextView itemview,
+      TextView hourview) {
     long currentTime = new Date().getTime();
     long startTime = mPrefs.getLong(Strings.LISTEN_START_TIME, currentTime);
-    float elapsedDays = ((float) currentTime - startTime) / DateUtils.DAY_IN_MILLIS + 1;
+    float elapsedDays = ((float) currentTime - startTime)
+        / DateUtils.DAY_IN_MILLIS + 1;
     int totalItems = mPrefs.getInt(Strings.LISTEN_TOTAL_ITEMS, 0);
     float itemsPerDay = totalItems / elapsedDays;
     long totalMillis = mPrefs.getLong(Strings.LISTEN_TOTAL_MILLIS, 0);
@@ -132,7 +176,8 @@ public class NavigationFragment extends ListFragment {
     hourview.setTextColor(getColorView(context, hoursPerDay * 2));
   }
 
-  private void setMonthSummary(Context context, TextView itemview, TextView hourview) {
+  private void setMonthSummary(Context context, TextView itemview,
+      TextView hourview) {
     int totalItems = mPrefs.getInt(Strings.LISTEN_MONTH_ITEMS, 0);
     long totalMillis = mPrefs.getLong(Strings.LISTEN_MONTH_MILLIS, 0);
     float totalHours = ((float) totalMillis) / DateUtils.HOUR_IN_MILLIS;
@@ -145,7 +190,8 @@ public class NavigationFragment extends ListFragment {
   /**
    * Total items and hours listened so far.
    */
-  private void setTotalSummary(Context context, TextView itemview, TextView hourview) {
+  private void setTotalSummary(Context context, TextView itemview,
+      TextView hourview) {
     int totalItems = mPrefs.getInt(Strings.LISTEN_TOTAL_ITEMS, 0);
     long totalMillis = mPrefs.getLong(Strings.LISTEN_TOTAL_MILLIS, 0);
     float totalHours = ((float) totalMillis) / DateUtils.HOUR_IN_MILLIS;
@@ -155,7 +201,8 @@ public class NavigationFragment extends ListFragment {
     hourview.setTextColor(getColorView(context, totalHours * 2));
   }
 
-  private void setWeekSummary(Context context, TextView itemview, TextView hourview) {
+  private void setWeekSummary(Context context, TextView itemview,
+      TextView hourview) {
     int totalItems = mPrefs.getInt(Strings.LISTEN_WEEK_ITEMS, 0);
     long totalMillis = mPrefs.getLong(Strings.LISTEN_WEEK_MILLIS, 0);
     float totalHours = ((float) totalMillis) / DateUtils.HOUR_IN_MILLIS;
